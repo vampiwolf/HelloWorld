a12d9793dad080ef49f477a1de35d833ab05206a
jxd <xiaodong.ji@cn.alps.com>
2019-09-06 11:44:52 +0800

    新建了文件.c和.h

---
+++ include/test.h
@@ -0,0 +1,9 @@
+#ifndef	_Test_H_
+#define	_Test_H_
+
+#include<stdio.h>
+
+extern int add(int a, int b);
+
+
+#endif
---
+++ src/test.c
@@ -0,0 +1,10 @@
+#include"test.h"
+
+int main(){
+	int a = 0;
+	int b = 0;
+	int c = add(a, b);
+	printf("a + b = %d.\r\n", a);
+
+	return 0;
+}
